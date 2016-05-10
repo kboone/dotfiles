7c95fab9fc76c467d715882c2cb6b2388a3ca5ef
Kyle Boone <kboone@berkeley.edu>
2016-05-10 16:34:14 -0700

    Add script to install cmake

---
+++ bin/install_cmake.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Script for installing cmake on systems where you don't have root access.
+# cmake will be installed in $PACKAGE_DIR/cmake.
+# We download the binary linux x64 installer which seems to work well. The
+# source for cmake is massive.
+
+# exit on error
+set -e
+
+INSTALL_DIR=$PACKAGE_DIR/cmake
+TEMP_BUILD_DIR=$INSTALL_DIR/build
+
+# create our directories
+mkdir -p $INSTALL_DIR $TEMP_BUILD_DIR
+cd $TEMP_BUILD_DIR
+
+wget https://cmake.org/files/v3.5/cmake-3.5.2-Linux-x86_64.sh \
+    --no-check-certificate
+chmod +x cmake-3.5.2-Linux-x86_64.sh
+./cmake-3.5.2-Linux-x86_64.sh --prefix=$INSTALL_DIR --skip-license
+
+echo "$INSTALL_DIR/bin/cmake is now available."
