e0d89c75b59854881db4bbb7716fe09c30843935
Kyle Boone <kboone@berkeley.edu>
2016-05-10 16:55:45 -0700

    Update to tmux 2.2

--- bin/install_tmux.sh
+++ bin/install_tmux.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 # Script for installing tmux on systems where you don't have root access.
-# tmux will be installed in $KYLE_INSTALL_DIR/bin.
-# It's assumed that wget and a C/C++ compiler are installed.
+# tmux will be installed in $PACKAGE_DIR/tmux.
+# It's assumed that wget and a C/C++ compiler are installed. We build libevent
+# and ncurses manually to get recent versions.
 
 # exit on error
 set -e
 
-INSTALL_DIR=$KYLE_INSTALL_DIR
+INSTALL_DIR=$PACKAGE_DIR/tmux
+TEMP_BUILD_DIR=$INSTALL_DIR/build
 
-TMUX_VERSION=2.0
+TMUX_VERSION=2.2
 
 # create our directories
-mkdir -p $INSTALL_DIR $INSTALL_DIR/tmux_tmp
-cd $INSTALL_DIR/tmux_tmp
+mkdir -p $INSTALL_DIR $TEMP_BUILD_DIR
+cd $TEMP_BUILD_DIR
 
 # download source files for tmux, libevent, and ncurses
 
@@ -55,6 +57,6 @@ cp tmux $INSTALL_DIR/bin
 cd ..
 
 # cleanup
-rm -rf $INSTALL_DIR/tmux_tmp
+rm -rf $TEMP_BUILD_DIR
 
 echo "$INSTALL_DIR/bin/tmux is now available."
