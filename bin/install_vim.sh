d9d957f5aeca1e633b42adc5bb13a429dc1bbe13
Kyle Boone <kboone@berkeley.edu>
2016-05-09 17:37:39 -0700

    Use packages instead of a global .kyle_install directory

--- bin/install_vim.sh
+++ bin/install_vim.sh
@@ -1,43 +1,46 @@
 #!/bin/bash
 
 # Script for installing vim on systems where you don't have root access.
-# vim will be installed in $KYLE_INSTALL_DIR/bin.
+# vim will be installed in $PACKAGE_DIR/vim.
+# We will link against a python anaconda environment with the name vim. That
+# should be set up before this is run with:
+# conda create --name vim anaconda python=2
 # It's assumed that wget and a C/C++ compiler are installed.
 
 # exit on error
 set -e
 
-INSTALL_DIR=$KYLE_INSTALL_DIR
+INSTALL_DIR=$PACKAGE_DIR/vim
+TEMP_BUILD_DIR=$INSTALL_DIR/build
 
-# PYTHON_DIR=$KYLE_INSTALL_DIR/anaconda/lib/python2.7/config
-# export LDFLAGS="-L$KYLE_INSTALL_DIR/anaconda/lib"
+# Figure out which python to use. We want to use python from an anaconda
+# environment with the name "vim"
+source activate vim
+VIM_PYTHON=$(which python)
 
 # create our directories
-mkdir -p $INSTALL_DIR $INSTALL_DIR/vim_tmp
-cd $INSTALL_DIR/vim_tmp
+mkdir -p $INSTALL_DIR $TEMP_BUILD_DIR
+cd $TEMP_BUILD_DIR
 
 git clone https://github.com/vim/vim.git
 cd vim
 
+vi_cv_path_python=$VIM_PYTHON \
 ./configure --with-features=huge \
             --enable-multibyte \
             --enable-gui=gtk2 \
             --enable-luainterp \
             --enable-perlinterp \
             --enable-rubyinterp \
+            --enable-pythoninterp \
             --enable-cscope \
             --prefix=$INSTALL_DIR
-
-# Try to get the various interpreters for free, but don't worry about it if
-# they don't work. To really get python working properly, you need the
-# following line set up:
-# --with-python-config-dir=$PYTHON_DIR \
 
 make
 make install
 
 
 # cleanup
-rm -rf $INSTALL_DIR/vim_tmp
+rm -rf $TEMP_BUILD_DIR
 
 echo "$INSTALL_DIR/bin/vim is now available."
