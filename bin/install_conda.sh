a4f37a32b65f8e6c9c8df78e4c93c6042b670135
Kyle Boone <kboone@berkeley.edu>
2017-01-24 20:39:55 -0800

    Big push to python3

---
+++ bin/install_conda.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Script for installing conda on systems where you don't have root access.
+# conda will be installed in $PACKAGE_DIR/conda.
+# I assume that wget and a C/C++ compiler are installed.
+
+PYTHON_VERSION=3
+CONDA_VERSION=latest
+
+# only accessible to me
+umask 077
+
+# exit on error
+set -e
+
+INSTALL_DIR=$PACKAGE_DIR/conda
+TEMP_BUILD_DIR=$PACKAGE_DIR/conda_temp
+
+# Create our directories. Note that conda needs to create its directory
+# itself, so we don't make that one.
+mkdir -p $PACKAGE_DIR $TEMP_BUILD_DIR
+ORIG_DIR=$(pwd)
+cd $TEMP_BUILD_DIR
+
+CONDA_SCRIPT=Miniconda${PYTHON_VERSION}-${CONDA_VERSION}-Linux-x86_64.sh
+
+# Install conda and set up a default environment.
+wget http://repo.continuum.io/miniconda/$CONDA_SCRIPT
+bash $CONDA_SCRIPT -b -p $INSTALL_DIR
+
+# Add the new conda to our path.
+export PATH=$INSTALL_DIR/bin:$PATH
+
+# Clean up.
+cd $ORIG_DIR
+rm -rf $TEMP_BUILD_DIR
+
+echo "$INSTALL_DIR/conda is now available."
+echo "Reload your bashrc with '. ~/.bashrc' to update your path."
---
+++ bin/update_conda.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+# Script for updating conda.
+# This script sets up all the channels that I want and installs a good default
+# set of packages that I use on all systems.
+# Note that the current environment isn't changed, so this can be used to set
+# up either the default environment or any newly created one.
+#
+# On some old science computers, the versions of libc and things like that are
+# way out of date so some packages break (eg: iminuit). To resolve that, build
+# a local conda package. I check for local packages in this script and don't
+# overwrite them.
+# Steps:
+#   conda skeleton pypi iminuit
+#   conda build iminuit
+#   conda install --use-local iminuit
+
+
+# Make created files only accessible to me
+umask 077
+
+# Exit on error
+set -e
+
+# List of packages to install
+PACKAGES=(
+    # Core packages
+    python
+    conda
+    conda-build
+    jupyter
+    argcomplete
+    beautifulsoup4
+
+    # General coding packages
+    flake8
+    tqdm
+
+    # Data science packages
+    numpy
+    scipy
+    matplotlib
+    pandas
+    scikit-learn
+    iminuit
+    pystan
+
+    # Astronomy packages
+    astropy
+    extinction
+    sncosmo
+    sep
+    pyephem
+)
+
+function ignore_packages {
+    # Remove packages matching a given regex in conda list from the list of
+    # packages to install. This is used to override updating manually installed
+    # packaged.
+    # Parameters are package name and conda list grep regex, 
+    IGNORE_PACKAGES=($(conda list | grep "$2" | awk '{print $1 "=" $2}'))
+
+    did_print=0
+    
+    for ignore_package in ${IGNORE_PACKAGES[@]}; do
+        index=0
+        for package in ${PACKAGES[@]}; do
+            if [ "$package" = "${ignore_package%=*}" ]; then
+                if [ "$did_print" -eq "0" ]; then
+                    echo "WARNING: some $1 packages are installed. These" \
+                        "will not be kept updated."
+                    did_print=1
+                fi
+                echo " - $ignore_package"
+                # Ugly. Set the object at the given index in the array to an
+                # empty variable and then remake the array. There is probably a
+                # better way of doing this.
+                unset PACKAGES[$index]
+                PACKAGES=(${PACKAGES[@]})
+                break
+            fi
+            index=$((index+1))
+        done
+    done
+
+    if [ $did_print -eq "1" ]; then
+        echo ""
+    fi
+}
+
+ignore_packages 'local' 'local$'
+ignore_packages 'pip' '<pip>$'
+
+# Update/install all packages that I want.
+conda install ${PACKAGES[@]}
