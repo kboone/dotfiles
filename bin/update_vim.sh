ddc1455c74170eaf7a75b2429b61739a5b8cf3dd
Kyle Boone <kboone@berkeley.edu>
2018-06-01 17:24:43 -0700

    vim: Remove YouCompleteMe hooks in updating

--- bin/update_vim.sh
+++ bin/update_vim.sh
@@ -4,7 +4,3 @@
 
 # First, download newest versions of all plugins.
 vim -c PlugUpdate -c PlugUpgrade -c PlugClean! -c quitall
-
-# Compile YouCompleteMe
-cd $HOME/.vim/plugged/YouCompleteMe
-./install.py
